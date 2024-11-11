dcdccf9bd1c8017485475389bb32a602af7633b4
Manuel Kilzer <manuel@kilzer.dev>
2024-11-11 03:36:58 +0100

    In CSS die warnings wieder eingefügt. Kleinere Anpassungen.

--- login-form.php
+++ login-form.php
@@ -34,45 +34,39 @@
                     <label for="password">Passwort:</label>
                     <input type="password" id="password" name="password" required>
                     <button type="submit">Anmelden</button>
-
-                    <?php
-                        // SQL datenabruf
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            $user = $_POST['username'];
-                            $passwort = $_POST['password'];
-                            $sql = "SELECT * FROM Userlogin WHERE User = :user";
-                            $stmt = $pdo->prepare($sql);
-                            $stmt->bindParam(':user', $user);
-                            $stmt->execute();
-                            $userData = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                            if ($userData && password_verify($passwort, $userData['Passwort'])) {
-                                // rich-tige eingaben
-                                if ($userData['Status'] === 'member') {
-                                    echo "Willkommen, " . htmlspecialchars($user) . "!";
-                                    $_SESSION['UserID'] = $userData['UserID'];
-
-                                    if ($_SESSION["isLoggedIn"] == false) {
-                                        $_SESSION["isLoggedIn"] = true;
-                                    }
-
-                                    header("Location: dashboard_test.php");
-                                } else {
-                                    echo "Ihr Konto ist derzeit " . htmlspecialchars($userData['Status']) . ".<br>";
-                                    echo "Warten Sie bitte auf die Freischaltung durch einen Administrator.";
-                                }
-                            } else {
-                                // falsche eingaben
-                                echo '<p class="warning">Benutzername oder Passwort ist falsch!</p>';
-                            }
-                        }
-                    ?>
                 </form>
+                <?php
+                    // SQL datenabruf
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        $user = $_POST['username'];
+                        $passwort = $_POST['password'];
+                        $sql = "SELECT * FROM Userlogin WHERE User = :user";
+                        $stmt = $pdo->prepare($sql);
+                        $stmt->bindParam(':user', $user);
+                        $stmt->execute();
+                        $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+                        if ($userData && password_verify($passwort, $userData['Passwort'])) {
+                            // Valide eingaben
+                            if ($userData['Status'] === 'member') {
+                                $_SESSION['UserID'] = $userData['UserID'];
+                                if ($_SESSION["isLoggedIn"] == false) {
+                                    $_SESSION["isLoggedIn"] = true;
+                                }
+                                header("Location: dashboard_test.php");
+                            } else {
+                                // User gesperrt
+                                echo '<p class="warning">Ihr Konto ist derzeit ' . htmlspecialchars($userData['Status']) . '!<br>Warte bitte auf die Freischaltung durch einen Administrator.</p>';
+                            }
+                        } else {
+                            // Eingaben Falsch
+                            echo '<p class="warning">Benutzername oder Passwort ist falsch!</p>';
+                        }
+                    }
+                ?>
                 <p>
                     Noch kein Konto? <a href="register-form.php">Registrieren</a>
                 </p>
             </div>
-
             <div class="xlogo">
                 <img src="images/xlogo_bg.png">
             </div>
