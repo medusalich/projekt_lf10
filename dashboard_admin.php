0cdffde078c136913144f6cc45f334e2db1a8b0b
Stephanie Hartwig <stephanie.blestel@gmail.com>
2024-11-11 21:33:16 +0100

    Admin Dashboard listet Name und Status der Mitarbeiter

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -1,14 +1,13 @@
 <?php
     require "db.php";
 
-    $sqlMitarbeiter = "SELECT vorname, nachname FROM Mitarbeiter WHERE MitarbeiterID = :MitarbeiterID";
+    $sqlMitarbeiter = "SELECT Mitarbeiter.vorname, Mitarbeiter.nachname, Userlogin.Status FROM Mitarbeiter JOIN Userlogin On Mitarbeiter.MitarbeiterID = Userlogin.UserID";
     $stmtMitarbeiter = $pdo->prepare($sqlMitarbeiter);
-    $stmtMitarbeiter->bindParam(':MitarbeiterID', $mitarbeiterID);
     $stmtMitarbeiter->execute();
 
     if ($stmtMitarbeiter->rowCount() > 0) {
         while ($row = $stmtMitarbeiter->fetch(PDO::FETCH_ASSOC)) {
-            echo "Nachname: " . $row["nachname"] . "Vorname: " . $row["vorname"] . "<br>";
+            echo "Nachname: " . $row["nachname"] . " Vorname: " . $row["vorname"] . " Status: " . $row["Status"] . "<br>";
         }
     }
 ?>
