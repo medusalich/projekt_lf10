40be33901e986717f7a6e622ea5b70242c4ac367
Stephanie Hartwig <stephanie.blestel@gmail.com>
2024-11-13 11:31:42 +0100

    title umbenannt

--- www/dashboard_admin.php
+++ www/dashboard_admin.php
@@ -51,7 +51,7 @@ $stmtMitarbeiter->execute();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin - Mitarbeiterverwaltung</title>
+    <title>Adminbereich X Logistics</title>
     <link rel="stylesheet" href="css/styles.css">
     <link rel="icon" href="images/favicon.ico" type="image/x-icon">
     <style>
