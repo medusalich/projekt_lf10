2a0134ac3badabed38aa2e865adb62f5418dfb13
Manuel Kilzer <manuel@kilzer.dev>
2024-11-14 02:16:48 +0100

    Erstmal allgemeine Änderungen an den Lohnabrechnungsseiten.
    Einrückungen und PHP korrigiert.

--- www/lohnabrechnung.php
+++ www/lohnabrechnung.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-
 <?php
     require "db.php";
     // Session Start 
@@ -22,46 +15,53 @@
     // Übertragung der Daten in Assoziatives Array
     $time_sheet = array();
     foreach ($logs as $log) {
-        $year = substr($log["datum"], 0, 4);
-        $month = substr($log["datum"], 5, 2);
-        $time_sheet[$year][$month] = (int)$log["gesamtzeit"];
+    $year = substr($log["datum"], 0, 4);
+    $month = substr($log["datum"], 5, 2);
+    $time_sheet[$year][$month] = (int)$log["gesamtzeit"];
     } 
 ?>
 
-<h2>Auswahl Lohnabrechnung</h2>
-<p>Bitte Jahr und Monat auswählen:</p>
 
-<form action="tabelle_lohn.php" method="POST">
-
-    <label for="year">Jahr:</label>
-    <select id="year" name="year"> 
-      <!-- Nur Jahre mit Daten anzeigen --> 
-      <?php
-        foreach (array_keys($time_sheet) as $year) {
-        echo "<option value=" . $year . ">" . $year . "</option>";
-        }
-    ?>
-    </select>
-
-    <label for="month">Monat:</label>
-    <select id="month" name="month">
-    <option value="01">Januar</option>
-    <option value="02">Februar</option>
-    <option value="03">März</option>
-    <option value="04">April</option>
-    <option value="05">Mai</option>
-    <option value="06">Juni</option>
-    <option value="07">Juli</option>
-    <option value="08">August</option>
-    <option value="09">September</option>
-    <option value="10">Oktober</option>
-    <option value="11">November</option>
-    <option value="12">Dezember</option>
-    </select>
-
-    <input type="submit">
-</form>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/styles.css">
 
 
-</body>
+
+
+    </head>
+    <body>
+        <h2>Auswahl Lohnabrechnung</h2>
+        <p>Bitte Jahr und Monat auswählen:</p>
+        <form action="tabelle_lohn.php" method="POST">
+
+            <label for="year">Jahr:</label>
+            <select id="year" name="year"> 
+            <!-- Nur Jahre mit Daten anzeigen --> 
+            <?php
+                foreach (array_keys($time_sheet) as $year) {
+                    echo "<option value=" . $year . ">" . $year . "</option>";
+                }
+            ?>
+            </select>
+
+            <label for="month">Monat:</label>
+            <select id="month" name="month">
+                <option value="01">Januar</option>
+                <option value="02">Februar</option>
+                <option value="03">März</option>
+                <option value="04">April</option>
+                <option value="05">Mai</option>
+                <option value="06">Juni</option>
+                <option value="07">Juli</option>
+                <option value="08">August</option>
+                <option value="09">September</option>
+                <option value="10">Oktober</option>
+                <option value="11">November</option>
+                <option value="12">Dezember</option>
+            </select>
+            <input type="submit">
+        </form>
+    </body>
 </html>
