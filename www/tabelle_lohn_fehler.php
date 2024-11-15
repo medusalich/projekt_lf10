9cc9225a14b8ced3eddb81d72fe0d156f8a46d22
Ryzekuchen <ryzekuchen95@gmail.com>
2024-11-15 01:01:01 +0100

    Update tabelle_lohn_fehler.php

--- www/tabelle_lohn_fehler.php
+++ www/tabelle_lohn_fehler.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="refresh" content="5;url=zeiterfassung.php"> 
+        <meta http-equiv="refresh" content="5;url=lohnabrechnung.php"> 
         <title>Lohntabelle X Logistics</title>
         <link rel="stylesheet" href="css/styles.css">
         <link rel="icon" href="images/favicon.ico" type="image/x-icon">
@@ -12,7 +12,7 @@
         <div class="message">
             <h1>Ungültige Auswahl!</h1>
             <p>In diesem Monat gibt es keine Arbeitszeiten. Bitte anderen Zeitraum wählen.</p>
-            <p> Du wirst gleich automatisch zum <a href="zeiterfassung.php">Dashboard</a> weitergeleitet.</p>
+            <p> Du wirst gleich automatisch zur <a href="lohnabrechnung.php">Auswahl</a> weitergeleitet.</p>
         </div>
     </body>
 </html>
